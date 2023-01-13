808b374e0e70c872cbac635255093e4dfa9bf843
unknown <wsialer@trabajo.gob.pe>
2023-01-12 23:04:18 -0500

    commit

--- src/main/java/com/practica/PracticaApplication.java
+++ src/main/java/com/practica/PracticaApplication.java
@@ -2,12 +2,11 @@ package com.practica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 //@EnableDiscoveryClient
 @SpringBootApplication
 public class PracticaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PracticaApplication.class, args);
     }
